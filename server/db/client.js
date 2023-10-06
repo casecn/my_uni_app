643367fb2895f64bbb751af6fccdfabe1fbfa3a2
Craig Case <case2cn@gmail.com>
2023-10-05 19:42:48 -0600

    connected app to remote postressql db.

--- server/db/client.js
+++ server/db/client.js
@@ -1,4 +1,9 @@
-const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/my_uni_app_db');
+const pg = require("pg");
+require("dotenv").config();
+console.log(process.env.DATABASE_URL);
+
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/my_uni_app_db"
+);
 
 module.exports = client;
